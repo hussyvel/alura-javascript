50f0e75c97baf43e373e7db60c273ea5fcbacca1
hussyvel <hussyribeiro@hotmail.com>
2020-07-31 19:31:07 -0300

    listando programas JavaScript

---
+++ loopswhiledo.js
@@ -0,0 +1,20 @@
+console.log("Testes de Loop While and Do" + "De 0 a 20")
+
+var number = 0;
+
+while (number <= 20){
+    console.log(number);
+    number += 2;
+}
+
+var total = 1;
+var contador = 0;
+
+console.log("Calculando 2 elevado a 10")
+//calculando 2 elevado a 10
+while(contador <= 10){
+    console.log(total)
+    total *= 2;
+    contador += 1
+}
+
